12d7f926060cfd8603cd2d29a92e235f27cb14ae
bustomi89 <32268437+bustomi89@users.noreply.github.com>
2017-10-11 10:35:18 +0700

    First commit
    First commit

---
+++ src/main/java/id/co/telkomsigma/belajarspringcache/BelajarSpringCacheApplication.java
@@ -0,0 +1,12 @@
+package id.co.telkomsigma.belajarspringcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringCacheApplication.class, args);
+	}
+}
